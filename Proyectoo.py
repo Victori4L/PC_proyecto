069043664dfc2ffd23e86a7fa481bcd6b7c7d475
Victori4L <142272451+Victori4L@users.noreply.github.com>
2023-08-25 15:09:04 -0600

    Avance 2

---
+++ Proyectoo.py
@@ -0,0 +1,7 @@
+
+nombre_materia= (input ("nombre de la materia :"))
+Cal_P1= float(input("Escribe calificación del primer parcial :"))
+Cal_P2= float(input("Escribe calificación del segundo parcial :"))
+Cal_P3= float(input("Escribe calificación del tercer parcial :"))
+Prom=(Cal_P1 + Cal_P2 + Cal_P3) /3.0
+print(f"La calificacion de la materia {nombre_materia} es= {Prom}")
