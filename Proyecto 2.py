0102556b1c57595443572c7bc4ef0e65449cc772
Victori4L <142272451+Victori4L@users.noreply.github.com>
2023-10-13 21:56:36 -0600

    Add files via upload

--- Proyecto 2.py
+++ Proyecto 2.py
@@ -1,23 +1,43 @@
 def calcular_promedio(lista):
-    suma=0
-    for indice in range(1,len(lista)):
-        suma=suma+lista[indice] 
-    return suma/(len (lista)-1)
+    suma = 0
+    for indice in range(1, len(lista)):
+        suma += lista[indice]
+    return suma / (len(lista) - 1)
+
 def obtener_calificaciones(materias):
-    alumno=str(input("Escribe el nombre del alumno: "))
-    lista=[alumno]
+    alumno = input("Escribe el nombre del alumno: ")
+    lista = [alumno]
     for nombre in materias:
-        calificaciones=float (input(f"Escribe la calificación de {nombre}: "))
+        calificaciones = float(input(f"Escribe la calificación de {nombre}: "))
         lista.append(calificaciones)
-    return lista 
-def main () :
-    repetir= input ("¿Quieres calcular el promedio de una materia? (s/n): ")
-    materias=["matematicas","espanol"]
-    while repetir== "s":
-        lista=obtener_calificaciones(materias)
-        print (f"La calificacion del alumno {lista[0]} es : {calcular_promedio(lista)}")
-        repetir=input ("¿Quieres calcular el promedio de otra materia?(s/n): ")
-        
-    print ("Adios")
+    return lista
+
+def main():
+    repetir = input("¿Quieres calcular el promedio de las materias? (s/n): ")
+    materias = ["matematicas", "espanol"]
+    lista_alumnos = []
+
+    while repetir == "s":
+        lista = obtener_calificaciones(materias)
+        promedio = calcular_promedio(lista)
+        lista_alumnos.append(lista)
+
+        print(f"La calificación del alumno {lista[0]} es: {calcular_promedio(lista)}")
+        if promedio >= 7.0:
+            print("El alumno ha aprobado.")
+        else:
+            print("El alumno ha reprobado.") 
+
+        repetir = input("¿Quieres calcular el promedio de otra materia? (s/n): ")
+
+    print("\nResumen de Promedios de Cada Alumno:")
     
+    for alumno in lista_alumnos:
+        nombre_alumno = alumno[0]
+        promedio_alumno = calcular_promedio(alumno)
+        print(f"Alumno: {nombre_alumno}, Promedio: {promedio_alumno}")
+
+    print("Adiós")
+
+
 main()
